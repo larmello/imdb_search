0fd3ee5561f66c2fb4cacd64cdaca76d2e1d08aa
Arthur Mello <arthur.mello@engenharia.ufjf.br>
2020-12-07 09:40:56 -0300

    yaml

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+require "yaml"
+
+file = "./imdb.yml"
+sample = YAML.load(File.read())
+
+puts 'Creating directors...'
+directors = {}  # slug => Director
+sample["directors"].each do |director|
+  directors[director["slug"]] = Director.create! director.slice("first_name", "last_name")
+end
+
+puts 'Creating movies...'
+sample["movies"].each do |movie|
+  Movie.create! movie.slice("title", "year", "synopsis").merge(director: directors[movie["director_slug"]])
+end
+
+puts 'Creating tv shows...'
+sample["series"].each do |tv_show|
+  TvShow.create! tv_show
+end
+puts 'Finished!'
